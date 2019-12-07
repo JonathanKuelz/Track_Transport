9dffd85befe099533963420f4620421c45864e8b
JonathanKuelz <jonathankuelz@tum.de>
2019-12-07 22:50:21 +0100

    Finished Filters

--- process_kml.py
+++ process_kml.py
@@ -16,14 +16,14 @@ def process_trackstring(track):
     matches = {'Date': '', 'Transport': [], 'Distance': []}
     regex_dict = {
         'Date': re.compile('(?<=Location history from )([0-9]{4}-[0-9]{2}-[0-9]{2})(?= to [0-9]{4}-[0-9]{2}-[0-9]{2})'),
-        'Transport': re.compile('(?<=\"Category\"><value>)(Running|Walking|On the subway|On a bus|Moving|Driving)(?=<)'),
+        'Transport': re.compile('(?<=\"Category\"><value>)'
+                                '(Running|Walking|On the subway|On a bus|Moving|Driving|On a tram|On a train)(?=<)'),
         'Distance': re.compile('(?<=[a-zA-Z]</value></Data><Data name=\"Distance\"><value>)[1-9][0-9]*')
     }
     matches['Date'] = re.findall(regex_dict['Date'], track)
     matches['Transport'] = re.findall(regex_dict['Transport'], track)
     matches['Distance'] = re.findall(regex_dict['Distance'], track)
-    # if len(matches['Transport']) is not len(matches['Distance']):
-    #     return False
+    assert len(matches['Transport']) == len(matches['Distance']), "Different number for transports and distances"
     return matches
 
 
@@ -36,5 +36,3 @@ def process(filepath):
 
     kml = read_kml(filepath)
     matches = process_trackstring(kml)
-
-    return matches
