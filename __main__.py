78c747cd13f8538b386fde4ec5806b06203f1ed1
JonathanKuelz <jonathankuelz@tum.de>
2019-12-07 21:34:15 +0100

    Minor Changes

--- __main__.py
+++ __main__.py
@@ -1,3 +1,3 @@
 from process_kml import process
 
-process('./Bewegungsdaten/history-2019-11-16.kml')
+tracking_data = process('./Bewegungsdaten/history-2019-11-16.kml')
--- process_kml.py
+++ process_kml.py
@@ -15,7 +15,7 @@ def read_kml(filepath):
 def process_trackstring(track):
     regex_dict = {
         'Date': re.compile("(?<=Location history from )([0-9]{4}-[0-9]{2}-[0-9]{2}) to ([0-9]{4}-[0-9]{2}-[0-9]{2})"),
-        'Motion': re.compile("(?<=<name>)(Running|Walking|On the subway)(?=</name>)")
+        'Motion': re.compile("(?<=<name>)(Running|Walking|On the subway|On a bus|)(?=</name>)")
     }
     for key in regex_dict:
         match = re.findall(regex_dict[key], track)
@@ -24,3 +24,17 @@ def process_trackstring(track):
 def process(filepath):
     kml = read_kml(filepath)
     process_trackstring(kml)
+
+    t1 = {
+        'Date', "2019-11-03"
+        'Transport', ["Walking", "Running", "On the subway"],
+        'Distance', [2, 13.2, 19]
+    }
+    t2 = {
+        'Date', "2019-12-05"
+                'Transport', ["On the subway", "Running", "On a bus"],
+        'Distance', [2, 0, 55]
+    }
+    timelines = [t1, t2]
+
+    return timelines
