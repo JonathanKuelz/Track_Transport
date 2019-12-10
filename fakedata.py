b7592bb16d9da871d6d3c3bc6b274bcf5f0512c0
Kevin Yuan <kevihiiin@users.noreply.github.com>
2019-12-10 11:05:54 +0100

    Fixed data generation & generated final datasets

--- fakedata.py
+++ fakedata.py
@@ -1,10 +1,11 @@
 import random
+from copy import deepcopy
 
-
-def augment_transport(emission_dl):
+def augment_transport(in_emission):
+    c_dl = deepcopy(in_emission)
     yearly_emission = 1000 * 11.61 / 365
     constant_emission = 0.75 * yearly_emission
-    for idx in range(len(emission_dl)):
+    for idx in range(len(c_dl)):
         variable_emission = 0.5 * random.random() * constant_emission
-        emission_dl[idx]['co2_emission'] += variable_emission + constant_emission
-    return emission_dl
+        c_dl[idx]['co2_emission'] += variable_emission + constant_emission
+    return c_dl
